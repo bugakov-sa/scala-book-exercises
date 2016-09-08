7b7279bf7ffa15d2040108c42684d74355fa602d
user <bugakov.sa.88@gmail.com>
2016-09-09 00:16:52 +0300

    chapter 13

--- src/main/scala/Chapter_13.scala
+++ src/main/scala/Chapter_13.scala
@@ -1,13 +1,52 @@
-object Ex_1 extends App {
+object Chapter_13 extends App {
 
-  import collection.mutable._
+  _1
+  _2
+  _4
+  _5
+  _6
 
-  def indexes(s: String) =
-    s.zipWithIndex.foldLeft(Map[Char, Set[Int]]())(
-      (map, pair) => {
-        map(pair._1) = map.getOrElse(pair._1, SortedSet[Int]()) + pair._2
-        map
-      })
+  def _1: Unit = {
+    import collection.mutable._
 
-  println(indexes("Mississippi"))
+    def indexes(s: String) =
+      s.zipWithIndex.foldLeft(Map[Char, Set[Int]]())(
+        (map, pair) => {
+          map(pair._1) = map.getOrElse(pair._1, SortedSet[Int]()) + pair._2
+          map
+        })
+
+    println(indexes("Mississippi"))
+  }
+
+  def _2: Unit = {
+    def indexes(s: String) =
+      s.zipWithIndex.foldLeft(Map[Char, List[Int]]())(
+        (map, pair) => {
+          map + (pair._1 -> (map.getOrElse(pair._1, Nil) :+ pair._2))
+        }
+      )
+
+    println(indexes("Mississippi"))
+  }
+
+  def _4: Unit = {
+    def encode(s: Iterable[String], m: Map[String, Int]) = s.flatMap(m get _)
+
+    println(encode(Array("Tom", "Fred", "Harry"),
+      Map(("Tom" -> 3), ("Dick" -> 4), ("Harry" -> 5))))
+  }
+
+  def _5: Unit = {
+    def mkString(collection: Iterable[Any], start: String, delimiter: String, end: String) =
+      start + collection.reduceLeft(_ + delimiter + _) + end
+
+    println(mkString((3 to 9), "<", ",", ">"))
+  }
+
+  def _6: Unit = {
+    val prices = List(5.0, 20.0, 9.95)
+    val quantities = List(10, 2, 1)
+    println((prices zip quantities) map (Function.tupled(_ * _)) sum)
+  }
 }

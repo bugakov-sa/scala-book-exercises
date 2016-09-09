829e392ac38dcb2741f9309167876e4e9e63fc76
user <bugakov.sa.88@gmail.com>
2016-09-09 08:50:24 +0300

    chapter 12

---
+++ src/main/scala/Chapter_12.scala
@@ -0,0 +1,81 @@
+object Chapter_12 extends App {
+
+  e1
+  e2
+  e3
+  e4
+  e5
+  e6
+  e7
+  e8
+  e9
+  e10
+
+  def e1: Unit = {
+    def values(fun: (Int => Int), low: Int, high: Int) = (low to high) map (x => (x, fun(x)))
+
+    println(values(x => x * x, -5, 5))
+  }
+
+  def e2: Unit = {
+    def max(a: Array[Int]) = a reduceLeft (math.max(_, _))
+
+    println(max(Array(34, 2, 9, 0, 45)))
+  }
+
+  def e3: Unit = {
+    def factorial(n: Int) = (1 to n) reduceLeft (_ * _)
+
+    println(factorial(5))
+  }
+
+  def e4: Unit = {
+    def factorial(n: Int) = (1 to n).foldLeft(1)(_ * _)
+
+    println(factorial(0))
+    println(factorial(5))
+  }
+
+  def e5: Unit = {
+    def largest(fun: (Int) => Int, inputs: Seq[Int]) = inputs map fun max
+
+    println(largest(x => 10 * x - x * x, 1 to 10))
+  }
+
+  def e6: Unit = {
+    def largest(fun: (Int) => Int, inputs: Seq[Int]) = inputs reduceLeft ((l, r) => if (fun(l) > fun(r)) l else r)
+
+    println(largest(x => 10 * x - x * x, 1 to 10))
+  }
+
+  def e7: Unit = {
+    def ajustToPair(fun: (Int, Int) => Int) = (p: (Int, Int)) => fun(p._1, p._2)
+
+    println(ajustToPair(_ * _)((6, 7)))
+  }
+
+  def e8: Unit = {
+    val a1 = Array("qwe", "qwert", "q")
+    val a2 = Array(3, 5, 1)
+    println(a1.corresponds(a2)(_.length == _))
+  }
+
+  def e9: Unit = {
+    def corresponds[A, B](a1: Array[A], a2: Array[B], fun: (A, B) => Boolean) = a1.corresponds(a2)(fun)
+
+    val a1 = Array("qwe", "qwert", "q")
+    val a2 = Array(3, 5, 1)
+    println(corresponds(a1, a2, (x: String, y: Int) => x.length == y))
+  }
+
+  def e10: Unit = {
+    def unless(condition: => Boolean)(block: => Unit) = if (!condition) block
+
+    unless(1 > 2) {
+      println("1>2")
+    }
+    unless(3 > 2) {
+      println("3>2")
+    }
+  }
+}
--- src/main/scala/Chapter_13.scala
+++ src/main/scala/Chapter_13.scala
@@ -1,12 +1,12 @@
 object Chapter_13 extends App {
 
-  _1
-  _2
-  _4
-  _5
-  _6
+  e1
+  e2
+  e4
+  e5
+  e6
 
-  def _1: Unit = {
+  def e1: Unit = {
     import collection.mutable._
 
     def indexes(s: String) =
@@ -19,7 +19,7 @@ object Chapter_13 extends App {
     println(indexes("Mississippi"))
   }
 
-  def _2: Unit = {
+  def e2: Unit = {
     def indexes(s: String) =
       s.zipWithIndex.foldLeft(Map[Char, List[Int]]())(
         (map, pair) => {
@@ -30,21 +30,21 @@ object Chapter_13 extends App {
     println(indexes("Mississippi"))
   }
 
-  def _4: Unit = {
+  def e4: Unit = {
     def encode(s: Iterable[String], m: Map[String, Int]) = s.flatMap(m get _)
 
     println(encode(Array("Tom", "Fred", "Harry"),
       Map(("Tom" -> 3), ("Dick" -> 4), ("Harry" -> 5))))
   }
 
-  def _5: Unit = {
+  def e5: Unit = {
     def mkString(collection: Iterable[Any], start: String, delimiter: String, end: String) =
       start + collection.reduceLeft(_ + delimiter + _) + end
 
     println(mkString((3 to 9), "<", ",", ">"))
   }
 
-  def _6: Unit = {
+  def e6: Unit = {
     val prices = List(5.0, 20.0, 9.95)
     val quantities = List(10, 2, 1)
     println((prices zip quantities) map (Function.tupled(_ * _)) sum)
